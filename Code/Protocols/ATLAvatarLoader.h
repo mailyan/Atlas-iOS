9fd72d581163280316b122d8a9adca3e5a1aaf65
Hrachya <h.hakobyan@gavrgroup.com>
2016-04-19 16:23:09 +0400

    - Added ATLAvatarLoader

---
+++ Code/Protocols/ATLAvatarLoader.h
@@ -0,0 +1,13 @@
+//
+//  ATLAvatarLoader.h
+//  Atlas
+//
+//  Created by Hrachya on 4/19/16.
+//
+//
+
+#ifndef ATLAvatarLoader_h
+#define ATLAvatarLoader_h
+#import "ESh.h"
+
+#endif /* ATLAvatarLoader_h */˚˚
